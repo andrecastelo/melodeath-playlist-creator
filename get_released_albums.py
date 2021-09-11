17424ea3dab42c13b73d4264127addd513e0f37c
Andre Castelo <andrecastelo@gmail.com>
2021-09-11 19:52:13 -0300

    feat: add support for range of months and years

--- get_released_albums.py
+++ get_released_albums.py
@@ -1,22 +1,32 @@
 import metallum
 
+
 def get_released_albums(**kwargs):
+    year = kwargs.get("year", 2021)
+    year_from = kwargs.get("year_from", year)
+    year_to = kwargs.get("year_to", year)
+
+    month = kwargs.get("month", 1)
+    month_from = kwargs.get("month_from", month)
+    month_to = kwargs.get("month_to", month)
+
     albums = metallum.album_search(
-        title='',
+        title="",
         strict=False,
-        year_from=kwargs.get('year', 2021),
-        year_to=kwargs.get('year', 2021),
-        month_from=kwargs.get('month', 1),
-        month_to=kwargs.get('month', 1),
-        genre='melodic death metal',
-        types=[1]
+        year_from=year_from,
+        year_to=year_to,
+        month_from=month_from,
+        month_to=month_to,
+        genre="melodic death metal",
+        types=[1],
     )
 
     return [
         {
-            'artist': album.band_name,
-            'name': album.title,
-            'genre': album._details[2],
-            'date': album._details[3][-14:-4],
-        } for album in albums
+            "artist": album.band_name,
+            "name": album.title,
+            "genre": album._details[2],
+            "date": album._details[3][-14:-4],
+        }
+        for album in albums
     ]
